390f2db04a958a28bc95364afc163bc8daed7014
rulerpe <neetisthebest@gmail.com>
2024-03-04 11:39:43 -0800

    self cert only for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 import fs from 'fs';
 import path from 'path';
 
-const crtPath = path.resolve(
-  __dirname,
-  '/home/rulerpe/selfCert/localhostfrontend.crt',
-);
-const keyPath = path.resolve(
-  __dirname,
-  '/home/rulerpe/selfCert/localhostfrontend.key',
-);
+const isProduction = process.env.NODE_ENV === 'production';
+let httpsOptions = {};
+
+if (!isProduction) {
+  const crtPath = path.resolve(
+    __dirname,
+    '/home/rulerpe/selfCert/localhostfrontend.crt',
+  );
+  const keyPath = path.resolve(
+    __dirname,
+    '/home/rulerpe/selfCert/localhostfrontend.key',
+  );
+  httpsOptions = {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(crtPath),
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(keyPath),
-      cert: fs.readFileSync(crtPath),
-    },
+    https: isProduction ? false : httpsOptions,
     port: 5000,
   },
 });
