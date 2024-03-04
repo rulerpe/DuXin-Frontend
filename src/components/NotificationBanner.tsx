03621fbb3d98a711b7c1c0e892065d88533d1887
rulerpe <neetisthebest@gmail.com>
2024-03-03 19:55:11 -0800

    updated title icon, and fix notification styling bug

--- src/components/NotificationBanner.tsx
+++ src/components/NotificationBanner.tsx
@@ -1,32 +1,37 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useTranslation } from 'react-i18next';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { useNotification } from "../contexts/NotificationContext";
-import styles from "../styles/NotificationBanner.module.css";
+import { useNotification } from '../contexts/NotificationContext';
+import styles from '../styles/NotificationBanner.module.css';
 
 const NotificationBanner = () => {
-    const { notification, hideNotification } = useNotification();
-    const { t } = useTranslation()
-    useEffect(() => {
-        if (notification) {
-            const timer = setTimeout(() => {
-                hideNotification();
-            }, 5000)
-            return () => clearTimeout(timer)
-        }
-    }, [notification, hideNotification])
+  const { notification, hideNotification } = useNotification();
+  const { t } = useTranslation();
+  useEffect(() => {
+    if (notification) {
+      const timer = setTimeout(() => {
+        hideNotification();
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [notification, hideNotification]);
 
+  return (
+    <div
+      className={`${styles.notificationBanner} ${styles[notification?.type || '']} ${notification?.message ? styles.visible : ''}`}
+    >
+      <span className={styles.notificationMessage}>
+        {t(notification?.message || '')}
+      </span>
+      <button
+        className={styles.notificationCloseBtn}
+        onClick={hideNotification}
+      >
+        <FontAwesomeIcon icon={faTimes}></FontAwesomeIcon>
+      </button>
+    </div>
+  );
+};
 
-
-    return (
-        <div className={`${styles.notificationBanner} ${styles[notification?.type || '']} ${notification?.message ? styles.visible : ''}`}>
-            <span className={styles.notificationMessage}>{t(notification?.message || '')}</span>
-            <button className={styles.notificationCloseBtn} onClick={hideNotification}>
-                <FontAwesomeIcon icon={faTimes}></FontAwesomeIcon>
-            </button>
-        </div >
-    )
-}
-
-export default NotificationBanner
+export default NotificationBanner;
