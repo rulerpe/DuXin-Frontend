5f068ef57f3ecf59c9fce2f288758618fe2a06d7
rulerpe <neetisthebest@gmail.com>
2024-03-04 22:53:12 -0800

    remove console.log

--- src/utils/ga4.ts
+++ src/utils/ga4.ts
@@ -1,7 +1,6 @@
 import ga4 from 'react-ga4';
 
 const MEASUREMENT_ID = import.meta.env.VITE_MEASUREMENT_ID;
-console.log('MEASUREMENT_ID', MEASUREMENT_ID);
 const isProduction = process.env.NODE_ENV === 'production';
 
 export const init = () =>
